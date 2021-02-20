57b58c0151c19cd2216b575ff93c14d252dce792
jiapengfei <felix9ia@outlook.com>
2021-02-20 19:43:34 +0800

    study immutable variable, const and shadowing.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,41 @@ fn print() {
     println!("{{}}")
 }
 
+fn variable() {
+    // mutable variable
+    let mut a = 123;
+    println!("old a is {}", a);
+    a = 456;
+    println!("new a is {}", a);
+
+    // assign a variable twice, variable can rebind
+    let b = 123;
+    println!("old b is {}", b);
+    let b = 456;
+    println!("b is {}", b);
+
+    // const
+    const C: i32 = 123;
+    println!("C is {}", C);
+    // cannot be named the same as a constant
+    // let C = 456;
+
+    // integer of 64bit
+    let d: i64 = 64;
+    println!("old d is {}", d);
+    // unsigned integer
+    let d: u64 = 128;
+    println!("new d is {}", d);
+    // Shadowing
+    // new d is a Shadow of old d.
+
+    let mut s = "123";
+    println!("s is {}", s);
+    // expected `&str`, found `usize`
+    // s = s.len();
+}
+
 fn main() {
-    print()
+    print();
+    variable()
 }
